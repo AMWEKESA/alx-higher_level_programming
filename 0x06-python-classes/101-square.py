a96524a2d202d3d09591c072e4c47205f3b256dd
AMWEKESA <thonymsai1@gmail.com>
2023-10-25 18:27:10 +0300

    correction

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -20,6 +29,7 @@ class Square:
 
     @property
     def position(self):
+        """Get/set the current position of the square."""
         return (self.__position)
 
     @position.setter
@@ -31,11 +41,12 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-
     def area(self):
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
@@ -47,6 +58,7 @@ class Square:
             print("")
 
     def __str__(self):
+        """Define the print() representation of a Square."""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
@@ -54,4 +66,4 @@ class Square:
             [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:
                 print("")
-            return ("")
+        return ("")
--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+"""My square module"""
+
 
 class Square:
+    """defines a square"""
 
     def __init__(self, size=0):
+        """Create a Square
+        Args: size: length of a side of Square
+        """
         self.__size = size
 
     @property
     def size(self):
+        """"The propery of size as the len of a side of Square
+        Raises:
+            TypeError: if size != int
+            ValueErrorr: if size < 0
+        """
         return self.__size
 
     @size.setter
@@ -18,11 +29,14 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Get the area of a Square
+        Returns: The size squared
+        """
         return self.__size * self.__size
 
     def __le__(self, other):
         return self.area() <= other.area()
-    
+
     def __lt__(self, other):
         return self.area() < other.area()
 
